00f79f14f08be02427d0514d52944d34da6ec44e
madchan <madchan912@gmail.com>
2024-08-30 16:52:43 +0900

    user-service 분리

--- E-Commerce/common/src/main/java/com/sparta/ecommerce/common/config/SecurityConfig.java
+++ E-Commerce/common/src/main/java/com/sparta/ecommerce/common/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.sparta.ecommerce.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,10 +19,14 @@ public class SecurityConfig {
         http
                 .csrf().disable() // CSRF 보호 비활성화
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/public/**").permitAll() // 공개 엔드포인트 설정
-                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                        .anyRequest().permitAll() // 모든 요청에 대해 인증 없이 접근 허용
                 );
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
