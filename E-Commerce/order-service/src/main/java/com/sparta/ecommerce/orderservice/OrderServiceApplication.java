4d0fdc7de8c73020932ac1b9d7a78df424adc384
madchan <madchan912@gmail.com>
2024-09-07 00:24:10 +0900

    오더 서비스 정상화

--- E-Commerce/order-service/src/main/java/com/sparta/ecommerce/orderservice/OrderServiceApplication.java
+++ E-Commerce/order-service/src/main/java/com/sparta/ecommerce/orderservice/OrderServiceApplication.java
@@ -2,10 +2,12 @@ package com.sparta.ecommerce.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.sparta.ecommerce.userservice", "com.sparta.ecommerce.common"})
+@EntityScan(basePackages = {"com.sparta.ecommerce.orderservice", "com.sparta.ecommerce.userservice", "com.sparta.ecommerce.productservice"})
+@EnableJpaRepositories(basePackages = {"com.sparta.ecommerce.orderservice", "com.sparta.ecommerce.userservice", "com.sparta.ecommerce.productservice"})
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
