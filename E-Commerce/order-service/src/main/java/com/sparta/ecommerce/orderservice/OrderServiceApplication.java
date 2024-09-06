305f851190229eaaa4e7bf1c2a270bc2e4452b6a
madchan <madchan912@gmail.com>
2024-09-07 00:14:49 +0900

    common 서비스 참조

--- E-Commerce/order-service/src/main/java/com/sparta/ecommerce/orderservice/OrderServiceApplication.java
+++ E-Commerce/order-service/src/main/java/com/sparta/ecommerce/orderservice/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.sparta.ecommerce.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sparta.ecommerce.userservice", "com.sparta.ecommerce.common"})
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- E-Commerce/user-service/src/main/java/com/sparta/ecommerce/userservice/UserServiceApplication.java
+++ E-Commerce/user-service/src/main/java/com/sparta/ecommerce/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.sparta.ecommerce.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sparta.ecommerce.userservice", "com.sparta.ecommerce.common"})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
